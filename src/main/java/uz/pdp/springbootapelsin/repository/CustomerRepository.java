0e67842b60d8053bf45918324c15beb063de1fa5
Bahodir1603 <adhamjonovbahodir7@gmail.com>
2022-06-11 00:49:28 +0500

    Bahodir

--- src/main/java/uz/pdp/springbootapelsin/repository/CustomerRepository.java
+++ src/main/java/uz/pdp/springbootapelsin/repository/CustomerRepository.java
@@ -1,8 +1,9 @@
 package uz.pdp.springbootapelsin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import uz.pdp.springbootapelsin.entity.Category;
 import uz.pdp.springbootapelsin.entity.Customer;
-
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 }
